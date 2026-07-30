67da4a15bb59a8a01127e3828f40b689ba113edc
rccyx <oss@rccyx.com>
2026-07-30 11:35:15 +0100

    add custom yx result type for explicit error handling

--- src/error.hpp
+++ src/error.hpp
@@ -1,27 +1,54 @@
 #ifndef ASRYX_ERROR_HPP
 #define ASRYX_ERROR_HPP
 
+#include <concepts>
 #include <expected>
 #include <string>
+#include <type_traits>
 #include <utility>
 
-namespace asryx {
+namespace yx {
 
 struct Error
 {
   std::string message;
 };
 
-inline std::unexpected<Error> fail(std::string message)
+template <typename T> using Result = std::expected<T, Error>;
+
+[[nodiscard]] inline std::unexpected<Error> fail(std::string message)
 {
-  return std::unexpected(Error{.message = std::move(message)});
+  return std::unexpected(Error{
+      .message = std::move(message),
+  });
 }
 
-template <typename T> void ignore_failure(const std::expected<T, Error>& result)
+[[nodiscard]] inline std::unexpected<Error> fail(Error error)
 {
-  (void)result.has_value();
+  return std::unexpected(std::move(error));
 }
 
-} // namespace asryx
+[[nodiscard]] inline Result<void> ok()
+{
+  return Result<void>{};
+}
+
+template <typename T> [[nodiscard]] Result<std::decay_t<T>> ok(T&& value)
+{
+  using Value = std::decay_t<T>;
+
+  return Result<Value>(std::in_place, std::forward<T>(value));
+}
+
+template <typename T, typename... Args>
+  requires std::constructible_from<T, Args...>
+[[nodiscard]] Result<T> ok(Args&&... args)
+{
+  return Result<T>(std::in_place, std::forward<Args>(args)...);
+}
+
+template <typename T> void ignore_failure(const Result<T>&) noexcept {}
+
+} // namespace yx
 
 #endif // ASRYX_ERROR_HPP
